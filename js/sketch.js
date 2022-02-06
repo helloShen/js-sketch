9f19455c80a45084abcb5cac59d4a76b9543c1cc
helloShen <symantec__@hotmail.com>
2022-02-06 03:22:43 -0500

    add 1.background layer. 2.color palettes. 3.rubber

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,10 @@
 /* create grids in canvas */
 const initCanvas = function() {
     const canvas = document.querySelector('.canvas');
+    const background = document.querySelector('.background');
     /* clear old canvas if any */
     canvas.innerHTML = '';
+    background.innerHTML = '';
     /* create new canvas */
     let row = parseInt(document.querySelector('textarea.canvasRow').value);
     let column = parseInt(document.querySelector('textarea.canvasColumn').value);
@@ -19,44 +21,129 @@ const initCanvas = function() {
     }
     canvas.style['grid-template-columns'] = `repeat(${column}, 1fr)`;
     canvas.style['grid-template-rows'] = `repeat(${row}, 1fr)`;
+    background.style['grid-template-columns'] = `repeat(${column}, 1fr)`;
+    background.style['grid-template-rows'] = `repeat(${row}, 1fr)`;
     for (let i = 0; i < (row * column); i++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.draggable = false; // default value is already false
-        cell.addEventListener('mousedown', draw, false);
-        cell.addEventListener('mouseenter', draw, false); // mouseenter is better than mouseover
-        cell.addEventListener('dragstart', () => {}, false);
-        canvas.appendChild(cell);
+        canvas.appendChild(canvasCell());
+        background.appendChild(backgroundCell(i, column));
+    }
+};
+
+/* overlay canvas cells, transparent by default */
+const canvasCell = function() {
+    let cell = document.createElement('div');
+    cell.classList.add('canvas-cell');
+    cell.style['background-color'] = '#FFFFFF';
+    cell.style['z-index'] = 1;
+    cell.style.opacity = 0;
+    cell.draggable = false; 
+    cell.addEventListener('mousedown', draw, false);
+    cell.addEventListener('mouseenter', draw, false); // mouseenter is better than mouseover
+    return cell;
+};
+
+/* background cells */
+const backgroundCell = function(idx, column) {
+    let cell = document.createElement('div');
+    cell.classList.add('background-cell');
+    cell.style['z-index'] = -1;
+    const floor = Math.floor((idx / column));
+    if (floor % 2 === 0) {
+        if (idx % 2 === 0) {
+            cell.style['background-color'] = '#EBEBEB';
+        } else {
+            cell.style['background-color'] = '#FFFFFF';
+        }
+    } else {
+        if (idx % 2 === 0) {
+            cell.style['background-color'] = '#FFFFFF';
+        } else {
+            cell.style['background-color'] = '#EBEBEB';
+        }
+    }
+    cell.draggable = false;
+    return cell;
+};
+
+/* create backgroundSwitch div */
+const backgroundSwitch = function() {
+    const bs = document.querySelector('.backgroundSwitch');
+    bs.dataset.state = 'on'; // default: have the background layer
+    bs.classList.add('on');
+    bs.addEventListener('click', switchBackground, false);
+}
+
+/* click event handler for backgroundSwitch div */
+const switchBackground = function(e) {
+    const bs = e.target;
+    if (bs.dataset.state.localeCompare('on') === 0) {
+        bs.dataset.state = 'off';
+        bs.classList.remove('on');
+        const background = document.querySelector('.background');
+        background.style.opacity = 0;
+    } else {
+        bs.dataset.state = 'on';
+        bs.classList.add('on');
+        const background = document.querySelector('.background');
+        background.style.opacity = 1;
     }
 }
+
+const rubber = function() {
+    const rubber = document.querySelector('.rubber');
+    rubber.addEventListener('click', () => mode = 0, false);
+}
+
+/* color pelettes */
+const colors = ['#00A5E3', '#8DD7Bf', '#FF96C5', '#FF5768', '#FFBF65'];
+const colorPalette = function() {
+    const palette = document.querySelector('.colors');
+    for (let i = 0; i < colors.length; i++) {
+        let color = document.createElement('div');
+        color.classList.add('color');
+        color.style['background-color'] = colors[i];
+        color.addEventListener('click', () => { 
+            mode = 1;
+            currentColor = color.style['background-color']
+        }, false);
+        palette.appendChild(color);
+    }
+};
 
 /* mousedown event handler on canvas */
 const toDraw = function(e) {
     e.preventDefault(); // !IMPORTANT: inhibit some browser's default drag & drop behaviors
     isDrawing = true;
-}
+};
 
 /* mouseenter event handler on each cell */
 const draw = function(e) {
     e.preventDefault(); // !IMPORTANT: inhibit some browser's default drag & drop behaviors
     if (isDrawing) {
-        this.style['background-color'] = color;
+        if (mode === 1) {
+            this.style['background-color'] = currentColor;
+            this.style.opacity = 1;
+        } else {
+            this.style['background-color'] = '#FFFFFF';
+            this.style.opacity = 0;
+        }
     }
-}
+};
 
 /* mouseup event handler on whole document */
 const stopDrawing = function(e) {
     isDrawing = false;
     e.stopPropagation();
-}
-
+};
 
 
 
 
 /* variables */
-let color = 'black';
+let mode = 1; // 0: rubber, 1: pencil
+let currentColor = 'black';
 let isDrawing = false;
+
 /* canvas init */
 const initCanvasBtn = document.querySelector('.initCanvas');
 initCanvasBtn.addEventListener('click', initCanvas);
@@ -65,5 +152,10 @@ const canvas = document.querySelector('.canvas');
 canvas.draggable = false;
 canvas.addEventListener('mousedown', toDraw, true); // capture must be true. canvas must turn on "isDrawing" before cell capture the mousedown event.
 document.addEventListener('mouseup', stopDrawing, true); // document element handle this. no need to notify children.
+
+initCanvas();
+backgroundSwitch();
+rubber();
+colorPalette();
 
 
